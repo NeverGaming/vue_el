35bfc6a1d0918192cb440a0c6895d5a7e1de4b43
seddumf <seddumf@qq.com>
2024-12-05 11:27:14 +0800

    待办事项初版

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg">
+       要做什么？
+      </label>
+    </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off" 
+      v-model.trim.lazy="label"
+      class="input__lg"
+    />  
+    <button type="submit" class="btn btn__primary btn__lg">添加</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+        onSubmit() {
+            if(this.label ==="") {
+                return;
+            }
+            this.$emit("todo-added", this.label);
+            this.label = "";
+            console.log("怎么回事，input没有自动清除？原来TMD是防止添加重复项， 以为是可以清空输入框", this.label)
+        },
+        data() {
+            return {
+                label: "",
+            }
+        },
+    },
+  };
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./assets/reset.css";
+
+// 全局引入Element Plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css' // 引入Element Plus样式
+
+// 全局引入ECharts
+import * as echarts from 'echarts'
+import VueECharts from 'vue-echarts'
 
 createApp(App).mount('#app')
+
+// 全局注册ECharts组件
+App.component('v-chart', VueECharts)
+
+// 将ECharts挂载到Vue原型上，以便在组件中通过this.$echarts访问
+App.config.globalProperties.$echarts = echarts
+
+// 使用Element Plus
+App.use(ElementPlus)
+App.mount('#app')
